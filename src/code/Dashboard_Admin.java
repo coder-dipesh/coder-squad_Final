e56589975b1c1c5ae6fac4d324fd8470c4576c6a
coder-dipesh <dipeshsiwakoti112@gmail.com>
2021-09-10 10:12:23 +0545

    .xml file changes

--- src/code/Dashboard_Admin.java
+++ src/code/Dashboard_Admin.java
@@ -1,23 +1,19 @@
 package code;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import static java.util.Objects.requireNonNull;
-import java.util.Objects;
 
 public class Dashboard_Admin extends Application{
 
         @Override
         public void start(Stage stage) throws Exception {
 
-            Parent root = FXMLLoader.load(getClass().getResource("../resource/dashboard_admin.fxml"));
+            Parent root = FXMLLoader.load(requireNonNull(getClass().getResource("../resource/dashboard_admin.fxml")));
             stage.setTitle("All IN ONE STORE - Admin Dashboard");
             stage.getIcons().add(new Image("src/img/icon.png"));
             stage.setScene(new Scene(root,1500,820));
